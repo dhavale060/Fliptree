deaf5bcc79d1431935b326c1d51ae80b70b4f436
dhavale060 <dhavales060@gmail.com>
2024-11-18 11:52:44 +0530

    design the cart item page

--- src/Pages/CartPage.js
+++ src/Pages/CartPage.js
@@ -5,6 +5,27 @@ import Counter from '../Atoms/Counter';
 import DeleteIcon from '@mui/icons-material/Delete';
 import {Button} from '@mui/material';
 import {removeFromCart } from '../common/StateManagement/ProductSlice';
+import styled from 'styled-components';
+import TypeText from '../Atoms/TypeText';
+
+
+const CartPageContainer = styled.div`
+    width: 1052px;
+    height: auto;
+`
+
+const ProductDescription = styled.div`
+    width: 500px;
+    height: auto;
+    margin: 6px;
+`
+const CounterButtonContainer = styled.div`
+    width: 500px;
+    height: -webkit-fill-available;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
 
 const CartPage = () => {
   const {cartItems} = useSelector((state) => state.ProductDetails)
@@ -16,29 +37,31 @@ const CartPage = () => {
   };
 
   return (
-    <div>
-      <Typography variant="h4" sx={{ mt: 4, mb: 2 }}>
-        Your Cart
-      </Typography>
+    <CartPageContainer>
+      <TypeText margin="5px" text="Cart Items" fontWeight="bold" fontSize="20px"/>
       {cartItems.length === 0 ? (
         <Typography variant="body1">Your cart is empty!</Typography>
       ) : (
         <List>
           {cartItems.map((item, index) => (
             <ListItem key={index}>
-              <ListItemText
-                primary={item.title}
-                secondary={`Price: $${item.price}`}
-              />
-              <Counter price={item.price} cartItems={cartItems}/>
-              <Button onClick={() => removeCart(item.id)} color="primary" aria-label="increase">
-                 <DeleteIcon/>
-              </Button>
+              <ProductDescription>
+                <ListItemText
+                  primary={item.title}
+                  secondary={`Price: $${item.price}`}
+                />
+              </ProductDescription>
+              <CounterButtonContainer>
+                <Counter price={item.price} cartItems={cartItems}/>
+                <Button onClick={() => removeCart(item.id)} color="primary" aria-label="increase">
+                   <DeleteIcon/>
+                </Button>
+              </CounterButtonContainer>
             </ListItem>
           ))}
         </List>
       )}
-    </div>
+    </CartPageContainer>
   );
 };
 
